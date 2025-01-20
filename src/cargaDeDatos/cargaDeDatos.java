3a0f1c38e9da882d2be1869192c5353471e078f0
Javier Vera Flores <aczayacatl27@hotmail.com>
2025-01-19 18:52:09 -0600

    definiendo paketes de trabajo

---
+++ src/cargaDeDatos/cargaDeDatos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cargaDeDatos;
+
+/**
+ *
+ * @author aczay
+ */
+public class cargaDeDatos {
+    
+}
--- src/recomendacionHoteles/recomendacionHoteles.java
+++ src/recomendacionHoteles/recomendacionHoteles.java
@@ -2,14 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package viajeinteligentesd;
+package recomendacionHoteles;
 
 /**
  *
  * @author aczay
  */
-public class NuevaClasePrueba {
-    public static void main(String[] args) {
-        System.out.println("Esta es otra prueba");
-    }
+public class recomendacionHoteles {
+    
 }
