b9cb3689d90b1b0358f699d06effbd45846cf19c
Javier Vera Flores <aczayacatl27@hotmail.com>
2025-08-01 14:14:22 -0600

    Prueba de git pull 2025

---
+++ src/Principal/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+/**
+ *
+ * @author aczay
+ */
+public class Principal {
+    public static void main(String args[]){
+            System.out.println("pruebas para git pull");
+    }
+    
+}
